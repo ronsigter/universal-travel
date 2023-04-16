7a4db1d9cbc14a55bbf0e793ee5d8acfc2b7f945
Chester Mendoza <ronchester.mendoza@gmail.com>
2023-04-17 00:29:52 +0800

    passes function below

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,10 +1,22 @@
 import { LEGENDS } from "~/helpers";
 
-export const Dropdown = () => {
+interface DropdownProps {
+  name: string;
+  onChange: (data: { color: string; level: number }) => void;
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({ name, onChange }) => {
   return (
-    <div className="absolute left-10 top-10 z-50 flex w-52 flex-col rounded-2xl border-2 bg-white py-2">
-      {LEGENDS.map(({ name }) => (
-        <button key={name} className="px-4 py-2 text-left ">
+    <div className="absolute left-10 top-10 z-50 flex w-52 flex-col overflow-hidden rounded-2xl border-2 bg-white">
+      <div className="flex justify-center py-2">
+        <span className="text-lg font-bold">{name}</span>
+      </div>
+      {LEGENDS.map(({ name, color, level }) => (
+        <button
+          key={name}
+          className="px-4 py-2 text-left hover:bg-slate-400 hover:text-neutral-50"
+          onClick={() => onChange({ color, level })}
+        >
           {name}
         </button>
       ))}
--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -8,10 +8,17 @@ interface Planet {
   name: string;
 }
 
+type HandleOnChangeProps = (data: { color: string; level: number }) => void;
+
 export const Planet: React.FC<Planet> = (props) => {
   const { color, size, name } = props;
   const [isOpen, setIsOpen] = useState(false);
+  const [levelColor, setLevelColor] = useState({ color, level: 0 });
   const planetRef = useRef<HTMLDivElement>(null);
+
+  const handleOnChange: HandleOnChangeProps = ({ color, level }) => {
+    setLevelColor({ color, level });
+  };
 
   useOnClickOutside(planetRef, () => {
     setIsOpen(false);
@@ -19,13 +26,13 @@ export const Planet: React.FC<Planet> = (props) => {
 
   return (
     <div
-      className={`relative cursor-pointer ${size[0]} ${size[1]} rounded-full ${color} flex items-center justify-center`}
+      className={`relative cursor-pointer ${size[0]} ${size[1]} rounded-full ${levelColor.color} flex items-center justify-center `}
       onClick={() => setIsOpen(!isOpen)}
       role="presentation"
       ref={planetRef}
     >
-      <span>{name}</span>
-      {isOpen && <Dropdown />}
+      <span className="absolute -top-8 font-bold text-slate-50">{name}</span>
+      {isOpen && <Dropdown name={name} onChange={handleOnChange} />}
     </div>
   );
 };
